320835a16421a7f4743c23674730d046060a57c5
meitai2020 <104344325+meitai2020@users.noreply.github.com>
2022-11-04 08:02:42 +0000

    app7 p*2追加

---
+++ app7.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test2.db');
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  const message = "Hello world";
+  res.render('p_show', {mes:message});
+});
+
+app.get("/player",(req, res) => {
+  const message = "Hello world!!";
+  app.get("/db", (req, res) => {
+    db.serialize( () => {
+        db.all("select id, player.id, player.player_name, team.team_name as name2 from player inner join team on player.team_id=team.team_id;", (error, row) => {
+            if( error ) {
+                res.render('p_show', {mes:"エラーです"});
+            }
+            res.render('p_select', {data:row});
+        })
+    })
+})
+  res.render('p_show',{mes:message});
+  db.serialize( () => {
+        db.all("", (error, row) => {
+            if( error ) {
+                res.render('p_show', {mes:"エラーです"});
+            }
+            res.render('p_select', {data:row});
+        })
+    })
+
+});//
+  
+
+
+
+app.use(function(req, res, next) {
+  res.status(404).send('ページが見つかりません');
+});
+
+app.listen(8080, () => console.log("Example app listening on port 8080!"));
